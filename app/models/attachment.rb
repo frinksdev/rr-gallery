6a955d9eb34e8a909908e1de5c509dc72957ff8b
Francisco Ortega <jf.ortega@bxf.mx>
2022-08-25 14:42:48 -0500

    Model: attachments

---
+++ app/models/attachment.rb
@@ -0,0 +1,6 @@
+class Attachment < ApplicationRecord
+  belongs_to :album
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: %r/\Aimage\/.*\z/
+end
