18991f314124a8f6b660d62a09e9288acaf512e6
Francisco Ortega <jf.ortega@bxf.mx>
2022-08-25 14:44:43 -0500

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_19_204817) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_154754) do
   create_table "action_mailbox_inbound_emails", force: :cascade do |t|
     t.integer "status", default: 0, null: false
     t.string "message_id", null: false
@@ -23,8 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_19_204817) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_19_204817) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -52,10 +52,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_19_204817) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "images_file_name"
-    t.string "images_content_type"
-    t.bigint "images_file_size"
-    t.datetime "images_updated_at"
+  end
+
+  create_table "attachments", force: :cascade do |t|
+    t.integer "album_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
+    t.index ["album_id"], name: "index_attachments_on_album_id"
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
@@ -87,4 +94,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_19_204817) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "attachments", "albums"
 end
